5107f94b2ab2c30f1a9fb97c1015a150bd22f559
ABEL0206 <wagnerabel95@gmail.com>
2026-04-10 12:52:57 +0200

    Create Program.cs

---
+++ AutoNelkuliIskola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoNelkuliIskola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
