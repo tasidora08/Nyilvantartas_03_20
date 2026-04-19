390c114b39553469d39b806754938ec82e1bf8fe
ABEL0206 <wagnerabel95@gmail.com>
2026-04-19 10:06:42 +0200

    Base of the WPF version

--- AutoNelkuliIskolaWPF/UserControls/CreateInstructorView.xaml.cs
+++ AutoNelkuliIskolaWPF/UserControls/CreateInstructorView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using AutoNelkuliIskolaWPF.Pages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AutoNelkuliIskolaWPF
+namespace AutoNelkuliIskolaWPF.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreateInstructorView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateInstructorView : UserControl
     {
-        public MainWindow()
+        public CreateInstructorView()
         {
             InitializeComponent();
-            MainFrame.NavigationService.Navigate(new MainPage());
         }
     }
 }
